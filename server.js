685a05ac7d3e898ab5bf45cf024a23e7e2f7320f
Twigster <treshagaye@gmail.com>
2022-02-12 13:31:09 -0500

    added delete function-needs work

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 // create a route that the front end can request data from
 
 const notes = require("./db/db");
+const req = require('express/lib/request');
 
 // set an environment variable, use this port, if se, or default to PORT 80
 const PORT = process.env.PORT || 3001;
@@ -18,19 +19,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
 app.get('/api/notes', (req, res) => {
-    // GET method requires 2 args: 1. string descriibing route client wants to fetch from & 2. callback function to execute everytim route is accessed with GET request
     let results = notes;
-    // if (req.query) {
-    //   results = filterByQuery(req.query, results);
-    // }
+  
     res.json(results);
 });
 
+   
 app.post('/api/notes', (req, res) => {
     // req.body is where our incoming content will be 
     console.log(req.body);
@@ -46,23 +46,27 @@ app.post('/api/notes', (req, res) => {
     res.send(req.body);
 
 });
-    
+
 
 app.delete('/api/notes/:id', (req, res) => {
-    req.params;
     console.log(req.params);
-    // use for loop to search which index this note id is in, if does, save index in a variable then delete it
-    // use the splice, then rewrite in db.json (fs.writefilesync)
-    // look into indexof- a method taht we pass a value in and it returns the index or position of where the element is
-    // look into using filter to identify id to delete
-})
+    
+    const result = notes.filter(note => note.id !== req.params.id);
+    console.log(notes);
+    console.log(result);
+    // return result;
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(result));
+    // const updatedNotes = require("./db/db");
+    // res.send(updatedNotes);
+    res.send(result);
+
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
     
 })
-
-
   
 // chain the listen methods to the server
 app.listen(PORT, () => {
